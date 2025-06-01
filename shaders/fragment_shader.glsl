731f928e0ab28661878aee9747d996c9281c0751
olek-dot-com <120317212+olek-dot-com@users.noreply.github.com>
2025-06-01 17:17:06 +0200

    Delete shaders directory

--- shaders/fragment_shader.glsl
+++
@@ -1,8 +0,0 @@
-#version 330
-uniform sampler2D spriteTex;
-out vec4 fragColor;
-void main()
-{
-    fragColor = texture(spriteTex, gl_PointCoord);
-    if (fragColor.a < 0.1) discard; // przezroczystość tła
-}
